19b0729b537ce248a0b765c5e9162f9719797f5d
Robert Colin <cfflymolo@gmail.com>
2015-07-07 22:37:09 -0700

    Added helper methods for problem 3 relating to prime numbers

--- Helpers.hs
+++ Helpers.hs
@@ -4,3 +4,13 @@ fibonacci :: Int -> Int
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci x = fibonacci (x - 1) + fibonacci (x - 2)
+
+factors :: Int -> [Int]
+factors 0 = []
+factors n = takeWhile (<n) [ x | x <- [2..], n `mod` x == 0 ]
+
+isPrime :: Int -> Bool
+isPrime n = (length $ factors n) == 0
+
+primeFactors :: Int -> [Int]
+primeFactors n = filter (isPrime) $ factors n
